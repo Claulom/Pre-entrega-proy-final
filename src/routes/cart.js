7394729cc72d61cc7947fb6f91130cc0414dbd0a
Claudio Lombao <claulom42@gmail.com>
2022-06-22 19:34:57 -0300

    Lombao

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,27 +1,24 @@
-import express, { json } from 'express'
+import express from 'express'
 import Productos from '../container.js'
 
+const carts = new Productos("cart.txt")
 const cartRouter = express.Router();
 
-
-const carts = new Productos("cart.json")
-
 cartRouter.get('/', (req, res)=>{
-  res.json(cart.getAll())
+  carts.getAll()
+  .then(data => res.send (data))
 })
 
-
 cartRouter.post('/', (req, res)=>{
-    let obj = {...req.body, ...{productos: []}}
-    res.json(carts.save(obj))
-    
+    let obj = {...req.body, ...{ productos: []}}
+    res.json(carts.save(obj))    
 })
 
 cartRouter.post('/:id/products', (req, res)=>{
     const product = req.body
     const cartID = req.params.id
     const cart = carts.getById(cartID)
-    cart.product.push(product)
+    cart.products.push(product)
 
     const newObj = carts.editById(cart, cartID)
     res.json(newObj)
